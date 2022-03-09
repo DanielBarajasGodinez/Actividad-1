333356f5717dbe17c6d53955c7dae67bfb6d7f55
DanielBarajasGodinez <101232034+DanielBarajasGodinez@users.noreply.github.com>
2022-03-09 15:07:16 -0600

    Ejercicio

---
+++ archivo.js
@@ -0,0 +1,18 @@
+var num= Number(prompt("Digita un numero"))
+var total= num*2
+console.log("El resultado de",num,"multiplicado por 2 es:",total)
+
+var num1= Number(prompt("Digita un numero"))
+var num2= Number(prompt("Digita otro numero"))
+var resta= num1 - num2
+console.log("La resta de", num1,"-",num2,"es:",resta)
+
+var area= 3.1416*(5.3*5.3)
+console.log("El area de un circulo con un radio de 5.3 cm es:", area, "cm")
+
+var P= 5
+var D= 8
+var Dm= 6
+var arearombo= 8*6/2
+console.log("El area de un rombo con un perimetro de 5 cm, Diagonal Mayor de 8 cm  y Diagonal menor de 6 cm es:", arearombo, "cm")
+
